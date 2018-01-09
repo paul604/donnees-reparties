34bf1776d74ec52bcad954af94c2b588b02f8053
paul <paul604@users.noreply.github.com>
2018-01-09 11:48:30 +0100

    add insert

--- TD1.sql
+++ TD1.sql
@@ -56,4 +56,10 @@ DROP TABLE Adherent;
 /*
 *   Insert
 */
+INSERT INTO ADHERENT VALUES (0001, 'nom', 'prenom', 'addr');
+INSERT INTO ADHERENT VALUES (0002, 'GLaDOS', 'aperture', 'Michigan');
+INSERT INTO ADHERENT VALUES (0003, 'steve', 'craft', 'world');
+
+
+INSERT INTO LIVRE VALUES (00000042, 'cake is a live', 'apertureS', 2042, '', 0, TO_DATE('01/05/2043','DD/MM/YYYY'));
 
