29387b2e86c9d36332cbe6bc772053a0dc0f5b27
paul <paul604@users.noreply.github.com>
2018-01-11 15:47:04 +0100

    fix max_exem-emp

--- Triggers.sql
+++ Triggers.sql
@@ -8,6 +8,26 @@ BEGIN
         (SELECT EXEMPLAIREEMPRUNTE.NUMA, EXEMPLAIREEMPRUNTE.DATEE FROM EXEMPLAIREEMPRUNTE);
 END;
 
+
+
 /*
 * max ExemplaireEmprunte 5 by adherent
+*/
+create or replace TRIGGER max_exem_emp
+    AFTER INSERT on EXEMPLAIREEMPRUNTE
+    declare
+        ligne_exem_emp ADHERENT%rowtype;
+begin
+    SELECT * into ligne_exem_emp FROM ADHERENT 
+        WHERE (SELECT COUNT(*) from EXEMPLAIREEMPRUNTE 
+        WHERE ADHERENT.NUMA=EXEMPLAIREEMPRUNTE.NUMA)>=5;
+    raise_application_error (-20001, 'un adhérant a emprunté plus de 5 exemplaires');
+    exception
+        when no_data_found then null;
+        when too_many_rows then
+    raise_application_error (-20002, 'des adhérants ont emprunté plus de 5 exemplaires');
+end;
+
+/*
+* up nbE and dateDE in Livre where add in ExemplaireEmprunte
 */
