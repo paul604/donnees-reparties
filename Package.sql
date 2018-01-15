c8be2c03e38fb1eb1b138939d2b7a124350a7328
paul <paul604@users.noreply.github.com>
2018-01-15 12:10:39 +0100

    SupExemplaire

--- Package.sql
+++ Package.sql
@@ -22,6 +22,8 @@ PROCEDURE CreerLivre(
 TYPE tabNumInv is VARRAY(5) of NUMBER(4);
 PROCEDURE CreerExemplaire(tab_numInv tabNumInv, isbn NUMBER);
 
+PROCEDURE SupExemplaire(tab_numInv tabNumInv);
+
 END;
 
 /*
@@ -35,7 +37,7 @@ PROCEDURE CreerAdherent(num_ad NUMBER, nom_ad VARCHAR2, prenom_ad VARCHAR2, addr
         INSERT INTO ADHERENT VALUES (num_ad, nom_ad, prenom_ad, addr_ad);
         COMMIT;
         EXCEPTION
-        WHEN OTHERS  THEN
+        WHEN OTHERS THEN
             IF SQLCODE = -0001 THEN
                 RollBack;
                 RAISE_APPLICATION_ERROR(-20001, 'adherent existe déjà');
@@ -74,6 +76,27 @@ PROCEDURE CreerExemplaire(tab_numInv tabNumInv, isbn NUMBER) IS
             numInv := isbn || tab_numInv(i);
             INSERT INTO EXEMPLAIRE VALUES(numInv, isbn);
         END LOOP;
+        COMMIT;
+    END;
+    
+PROCEDURE SupExemplaire(tab_numInv tabNumInv) IS
+    numInvSup EXEMPLAIRE.NUMINV%TYPE;
+    Pas_exemplaire EXCEPTION;
+    BEGIN
+        SET TRANSACTION READ WRITE;
+        numInvSup := 0;
+        FOR i IN tab_numInv.first .. tab_numInv.last LOOP
+            numInvSup := tab_numInv(i);
+            DELETE FROM EXEMPLAIRE WHERE EXEMPLAIRE.numInv = numInvSup;
+            if SQL%NOTFOUND then
+                RAISE Pas_exemplaire;
+            END IF;
+        END LOOP;
+        COMMIT;
+        EXCEPTION
+        WHEN Pas_exemplaire THEN
+            RollBack;
+            RAISE_APPLICATION_ERROR(-20006, numInvSup || ' pas trouvée');
     END;
 
 END;
@@ -102,5 +125,17 @@ begin
     numInv(1) := '1';
     numInv.extend;
     numInv(2) := '2';
+    numInv.extend;
+    numInv(3) := '3';
     MAJ.CreerExemplaire(numInv, 666);
+end;
+-- SupExemplaire
+declare numInv MAJ.tabNumInv;
+begin
+    numInv := MAJ.tabNumInv();
+    numInv.extend;
+    numInv(1) := 6663;
+    numInv.extend;
+    numInv(2) := 6662;
+    MAJ.SupExemplaire(numInv);
 end;
