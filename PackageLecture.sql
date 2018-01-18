cc8e8e49f47aae7ad4f2f13dcf983987bf1a41d4
paul <paul604@users.noreply.github.com>
2018-01-18 09:14:36 +0100

    listEmpruntsForAdherent

--- PackageLecture.sql
+++ PackageLecture.sql
@@ -3,8 +3,10 @@
 */
 CREATE OR REPLACE PACKAGE Lecture IS
 
-TYPE liste_adh IS REF CURSOR;
-PROCEDURE listAdherent(liste out liste_adh);
+TYPE liste_Cursor IS REF CURSOR;
+PROCEDURE listAdherent(liste out liste_Cursor);
+
+PROCEDURE listEmpruntsForAdherent(idAdh NUMBER, liste out liste_Cursor);
 
 END;
 
@@ -12,3 +14,39 @@ END;
 * body Lecture
 */
 create or replace PACKAGE BODY Lecture IS
+
+PROCEDURE listAdherent(liste out liste_Cursor) IS
+    BEGIN
+        SET TRANSACTION READ ONLY;
+        OPEN liste FOR SELECT * FROM ADHERENT;
+        COMMIT;
+    END;
+
+PROCEDURE listEmpruntsForAdherent(idAdh NUMBER, liste out liste_Cursor) IS
+    BEGIN
+        SET TRANSACTION READ ONLY;
+        OPEN liste FOR 
+        SELECT Exemplaire.NUMINV, Livre.titre
+            FROM Livre, EXEMPLAIRE
+            WHERE Livre.ISBN = EXEMPLAIRE.ISBN
+                AND EXEMPLAIRE.numInv IN (
+                    SELECT numInv 
+                    FROM ExemplaireEmprunte
+                    WHERE numA = idAdh
+                );
+        COMMIT;
+    END;
+
+END;
+
+/*
+* Test
+*/
+--listAdherent
+VARIABLE liste REFCURSOR;
+EXECUTE Lecture.listAdherent(:liste);
+PRINT :liste;
+--listEmpruntsForAdherent
+VARIABLE liste REFCURSOR;
+EXECUTE Lecture.listEmpruntsForAdherent(3,:liste);
+PRINT :liste;
