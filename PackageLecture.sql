8a5dc88cd7d3d248c06dc8db33a796287c572e27
paul <paul604@users.noreply.github.com>
2018-01-18 10:34:16 +0100

    fix listLivre

--- PackageLecture.sql
+++ PackageLecture.sql
@@ -28,14 +28,14 @@ PROCEDURE listEmpruntsForAdherent(idAdh NUMBER, liste out liste_Cursor) IS
     BEGIN
         SET TRANSACTION READ ONLY;
         OPEN liste FOR 
-        SELECT Exemplaire.NUMINV, Livre.titre
-            FROM Livre, EXEMPLAIRE
-            WHERE Livre.ISBN = EXEMPLAIRE.ISBN
-                AND EXEMPLAIRE.numInv IN (
-                    SELECT numInv 
-                    FROM ExemplaireEmprunte
-                    WHERE numA = idAdh
-                );
+            SELECT Exemplaire.NUMINV, Livre.titre
+                FROM Livre, EXEMPLAIRE
+                WHERE Livre.ISBN = EXEMPLAIRE.ISBN
+                    AND EXEMPLAIRE.numInv IN (
+                        SELECT numInv 
+                        FROM ExemplaireEmprunte
+                        WHERE numA = idAdh
+                    );
         COMMIT;
     END; 
 
@@ -43,7 +43,20 @@ PROCEDURE listLivre(liste out liste_Cursor) IS
     BEGIN
         SET TRANSACTION READ ONLY;
         OPEN liste FOR 
-
+            SELECT ISBN, Titre,
+                (SELECT COUNT(*) 
+                    FROM EXEMPLAIRE ex 
+                    WHERE ex.ISBN = liv.ISBN
+                ) nb_exemplaire,
+                (SELECT COUNT(*)
+                    FROM EXEMPLAIRE ex
+                    WHERE ex.ISBN = liv.ISBN
+                        AND ex.NUMINV NOT IN (
+                            SELECT NUMINV 
+                            FROM EXEMPLAIREEMPRUNTE exEp
+                        )
+                ) nb_libre
+            FROM Livre liv;
         COMMIT;
     END; 
 
@@ -59,4 +72,8 @@ PRINT :liste;
 --listEmpruntsForAdherent
 VARIABLE liste REFCURSOR;
 EXECUTE Lecture.listEmpruntsForAdherent(3,:liste);
+PRINT :liste;
+--listLivre
+VARIABLE liste REFCURSOR;
+EXECUTE Lecture.listLivre(:liste);
 PRINT :liste;
