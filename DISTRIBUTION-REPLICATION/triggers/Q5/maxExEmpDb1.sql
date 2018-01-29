0237468ae9042b2d297b35f7bcd523cd47ac6cb0
paul <paul604@users.noreply.github.com>
2018-01-29 16:40:04 +0100

    clean

---
+++ DISTRIBUTION-REPLICATION/triggers/Q5/maxExEmpDb1.sql
@@ -0,0 +1,19 @@
+create or replace TRIGGER max_exem_emp
+    AFTER INSERT on EXEMPLAIREEMPRUNTE
+    declare
+        ligne_exem_emp ADHERENT%rowtype;
+begin
+    SELECT * into ligne_exem_emp FROM ADHERENT 
+        WHERE (
+            (SELECT COUNT(*) FROM EXEMPLAIREEMPRUNTE exN
+                WHERE ADHERENT.NUMA=exN.NUMA)
+            +
+            (SELECT COUNT(*) FROM EXEMPLAIREEMPRUNTE@vers_Angers exA
+                WHERE ADHERENT.NUMA=exA.NUMA)
+        )>5;
+    raise_application_error (-20001, 'un adhérant a emprunté plus de 5 exemplaires');
+    exception
+        when no_data_found then null;
+        when too_many_rows then
+    raise_application_error (-20002, 'des adhérants ont emprunté plus de 5 exemplaires');
+end;
